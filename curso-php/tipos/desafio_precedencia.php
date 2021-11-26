0da103536e9e65d3d3ab13e7261137f4236b576a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-26 15:53:05 -0300

    Desafio precedência

---
+++ curso-php/tipos/desafio_precedencia.php
@@ -0,0 +1,14 @@
+<div class="titulo">Desafio precedência</div>
+<?php
+
+echo '<p>1. Qual o valor será impresso?</p>';
+echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3);
+
+echo '<p>2. Qual expressão imprime o valor 100?</p>';
+echo 'a) ', (1 + 2) * 20 - 15;
+echo '<br>';
+echo 'b) ', 4 * 5 ** 2;
+echo '<br>';
+echo 'c) ', 2 ** 3 ** 4 / 1e25;
+echo '<br>';
+echo 'd) ', 3 + (3 * 8) / 2 - 3;
