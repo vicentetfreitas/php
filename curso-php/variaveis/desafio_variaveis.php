79bdb301d9c28a0bff43eb9ede289d1330452dd5
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-30 21:28:15 -0300

    Desafio variáveis variáveis

---
+++ curso-php/variaveis/desafio_variaveis.php
@@ -0,0 +1,11 @@
+<div class="titulo">Desafio variáveis</div>
+
+<?php
+$a = 'Nossa';
+$nossa = 'Eu';
+$Eu = 'consegui';
+$consegui = 'responder';
+$responder = 'esse';
+$esse = 'desafio';
+
+echo "$a! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}";
