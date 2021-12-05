bad6a8f3ff735878f3b26bea840203d9c3030761
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-12-05 09:11:57 -0300

    Operador ternário

--- curso-php/index.php
+++ curso-php/index.php
@@ -28,6 +28,7 @@
             <li> <a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></a></li>
             <li> <a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos</a></a></li>
             <li> <a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio operadores lógicos</a></a></li>
+            <li> <a href="exercicio.php?dir=controle&file=operador_ternario">Operador ternário</a></a></li>
           </ul>
         </div>
         <div class="modulo azul">
