000cc1e34e244bb586399ba07eca97fa35be128e
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-12-20 21:32:31 -0300

    Switch

--- curso-php/index.php
+++ curso-php/index.php
@@ -29,6 +29,7 @@
             <li> <a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos</a></a></li>
             <li> <a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio operadores lógicos</a></a></li>
             <li> <a href="exercicio.php?dir=controle&file=operador_ternario">Operador ternário</a></a></li>
+            <li> <a href="exercicio.php?dir=controle&file=switch">Switch</a></a></li>
           </ul>
         </div>
         <div class="modulo azul">
